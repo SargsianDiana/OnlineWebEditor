1d1cc44447442db2b374a6ba8d1c19ae44e1e314
Hamlet <67255886+HamletGal@users.noreply.github.com>
2020-07-30 16:23:33 +0400

    change files

--- Custom-Online-Web-Editor/App/RightClick.js
+++ Custom-Online-Web-Editor/App/RightClick.js
@@ -1,43 +1,59 @@
+import Fire from "./../firebase/config.js"
+import MyList from "./createMyList.js"
+
 let menu = null;
-document.addEventListener('DOMContentLoaded', function () {
-  //make sure the right click menu is hidden
-  menu = document.querySelector('.menu');
-  menu.classList.add('off');
-
-  //add the right click listener to the box
-  let box = document.getElementsByClassName('box');
-  for (let i = 0; i < box.length; i++) {
-    box[i].addEventListener('contextmenu', showmenu);
-  }
-
-  //add a listener for leaving the menu and hiding it
-  menu.addEventListener('mouseleave', hidemenu);
-
-  getValue();
+document.addEventListener('DOMContentLoaded', function(){
+    //make sure the right click menu is hidden
+    menu = document.querySelector('.menu');
+    menu.classList.add('off');
+    
+    //add the right click listener to the box
+    let box = document.getElementsByClassName('box');
+    for(let i = 0;i < box.length; i++){
+        box[i].addEventListener('contextmenu', showmenu);
+    }
+    
+    //add a listener for leaving the menu and hiding it
+    menu.addEventListener('mouseleave', hidemenu);
+    
+    getValue()
 });
 
-function getValue() {
-  document.getElementById('setValue').onclick = function () {
-    let t = document.getElementById(inputBox.value).id;
-    alert(t);
-  };
+function getValue(){
+    document.getElementById("setValue").onclick = function(){
+        let id = document.getElementById(inputBox.value).id
+        var adaRef = Fire.database().ref(`Menu1/${id}`);
+        adaRef.remove()
+          .then(function(){
+            let myList = new MyList()
+            myList.createMyList()
+          })  
+          .then(function() {
+            console.log("Remove succeeded.")
+          })
+          .catch(function(error) {
+            console.log("Remove failed: " + error.message)
+          });
+    }
 }
 
-function showmenu(ev) {
-  //stop the real right click menu
-  ev.preventDefault();
-  //show the custom menu
-  inputBox.value = ev.target.id;
-  console.log(ev.clientX, ev.clientY);
-  menu.style.top = `${ev.clientY - 20}px`;
-  menu.style.left = `${ev.clientX - 20}px`;
-  menu.classList.remove('off');
+function showmenu(ev){
+    //stop the real right click menu
+    ev.preventDefault(); 
+    //show the custom menu
+    inputBox.value = ev.target.id
+    console.log( ev.clientX, ev.clientY );
+    menu.style.top = `${ev.clientY - 20}px`;
+    menu.style.left = `${ev.clientX - 20}px`;
+    menu.classList.remove('off');
 }
 
-function hidemenu(ev) {
-  menu.classList.add('off');
-  menu.style.top = '-200%';
-  menu.style.left = '-200%';
+function hidemenu(ev){
+    menu.classList.add('off');
+    menu.style.top = '-200%';
+    menu.style.left = '-200%';
 }
 
-export default 0;
+
+
+export default 0

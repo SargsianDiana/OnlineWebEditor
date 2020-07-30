bd686698f10c048b7a7a1757e052a5d00ec23bf4
Marine Mirzabekyan <67229028+Marine22-M@users.noreply.github.com>
2020-07-30 20:49:27 +0400

    Btn_Img

--- Custom-Online-Web-Editor/App/Elements.js
+++ Custom-Online-Web-Editor/App/Elements.js
@@ -1,6 +1,55 @@
-import fileObj from './File.js';
-import folderObj from './Folder.js';
-import collapseObj from './Collapse.js';
+import fileObj from "./File.js";
+import folderObj from "./Folder.js";
+import collapseObj from "./Collapse.js";
+
+// const createBtn = function (el, innerText, classList, cb) {
+//   const htmlEl = document.createElement(el);
+//   htmlEl.innerText = innerText;
+//   htmlEl.classList = classList;
+//   htmlEl.onclick = cb;
+
+//   const div = document.getElementById('folders');
+//   div.append(htmlEl);
+// };
+
+// createBtn('button', '+', 'actionBtn', function () {
+//   return fileObj.createInput();
+// });
+
+// createBtn('button', '++', 'actionBtn', function () {
+//   return folderObj.createInput();
+// });
+
+// createBtn('button', '*', 'actionBtn', function () {
+//   return collapseObj.hideToggle();
+// });
+
+// createBtn('button', '⊗ 0 ⚠ 0', 'terminalBtn', function () {
+//   return OpenTerminal();
+// });
+
+// export default 0;
+
+const createBtnImg = function (el, src, classList, cb) {
+  const htmlEl = document.createElement(el);
+  htmlEl.src = src;
+  htmlEl.classList = classList;
+  htmlEl.onclick = cb;
+  const div = document.getElementById("folders");
+  div.append(htmlEl);
+};
+
+createBtnImg("img", "./image/file.png", "actionImg", function () {
+  return fileObj.createInput();
+});
+
+createBtnImg("img", "./image/folderpl.png", "actionImg", function () {
+  return folderObj.createInput();
+});
+
+createBtnImg("img", "./image/collapse.jpg", "actionCollapse", function () {
+  return collapseObj.hideToggle();
+});
 
 const createBtn = function (el, innerText, classList, cb) {
   const htmlEl = document.createElement(el);
@@ -8,23 +57,11 @@ const createBtn = function (el, innerText, classList, cb) {
   htmlEl.classList = classList;
   htmlEl.onclick = cb;
 
-  const div = document.getElementById('folders');
+  const div = document.getElementById("folders");
   div.append(htmlEl);
 };
 
-createBtn('button', '+', 'actionBtn', function () {
-  return fileObj.createInput();
-});
-
-createBtn('button', '++', 'actionBtn', function () {
-  return folderObj.createInput();
-});
-
-createBtn('button', '*', 'actionBtn', function () {
-  return collapseObj.hideToggle();
-});
-
-createBtn('button', '⊗ 0 ⚠ 0', 'terminalBtn', function () {
+createBtn("button", "⊗ 0 ⚠ 0", "terminalBtn", function () {
   return OpenTerminal();
 });
 

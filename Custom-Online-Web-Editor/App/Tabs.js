bc182e21a926c1d486d314373e2a1aead7b99cdb
Hamlet <67255886+HamletGal@users.noreply.github.com>
2020-07-31 08:05:10 -0700

    update close tab

--- Custom-Online-Web-Editor/App/Tabs.js
+++ Custom-Online-Web-Editor/App/Tabs.js
@@ -5,11 +5,21 @@ class Tab{
         let tabItem = document.getElementById(id)
         let textContent = tabItem.textContent
         let createTabItem = document.createElement('div')
+        createTabItem.id = `tab_${id}`
+        let createTabClose = document.createElement('span')
+        createTabClose.id = 'tabClose'
+        createTabClose.onclick = (ev) => {
+            document.getElementById(ev.target.parentNode.id).remove()
+        }
+        createTabClose.innerHTML = 'x' 
         createTabItem.id = "tabs_Item"
         createTabItem.innerHTML = textContent
+        createTabItem.append(createTabClose)
         tabs.append(createTabItem)
+        
 
     }
+ 
     
 }
 
